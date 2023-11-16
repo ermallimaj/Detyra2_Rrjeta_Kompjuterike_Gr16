fafe6e31eb2ee1b262f4e2b40550eb95fee83e19
Erza Merovci <erzaamerovci@gmail.com>
2023-11-16 23:17:26 +0100

    Included libraries
    Included some necessary libraries and declared port number and ip addres for server.

--- Project/Project/server.cpp
+++ Project/Project/server.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <unistd.h>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 
 using namespace std;
 
 int main() {
+	
+
+	int portNumber = 12345;
+	const char* ipAddress = "127.0.0.1";
 
 	return 0;
 }
