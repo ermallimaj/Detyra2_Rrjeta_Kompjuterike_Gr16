0112bf2a4bbb1c1329e4262fd64660dde13f716e
ermallimaj <ermallimaj32@gmail.com>
2023-11-18 18:56:07 +0100

    Initial Setup

--- Project/Project/server.cpp
+++ Project/Project/server.cpp
@@ -1,15 +1,47 @@
 #include <iostream>
 #include <fstream>
 #include <winsock2.h>
-
 using namespace std;
 
-#define SERVER "127.0.0.1"
+#pragma comment(lib,"ws2_32.lib")
+#pragma warning(disable:4996)
+#define IP "127.0.0.1"
 #define BUFLEN 512
 #define PORT 8888
 
 int main()
 {
+    system("title UDP C++ Server Gr16");
 
-	return 0;
+    sockaddr_in server, client;
+
+    WSADATA wsa;
+    printf("Initialising Winsock...");
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+    {
+        printf("Failed. Error Code: %d", WSAGetLastError());
+        exit(0);
+    }
+    printf("Initialised.\n");
+
+    SOCKET server_socket;
+    if ((server_socket = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
+    {
+        printf("Could not create socket: %d", WSAGetLastError());
+    }
+    printf("Socket created.\n");
+
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(PORT);
+
+
+    if (bind(server_socket, (sockaddr*)&server, sizeof(server)) == SOCKET_ERROR)
+    {
+        printf("Bind failed with error code: %d", WSAGetLastError());
+        exit(EXIT_FAILURE);
+    }
+    puts("Bind done.");
+
 }
