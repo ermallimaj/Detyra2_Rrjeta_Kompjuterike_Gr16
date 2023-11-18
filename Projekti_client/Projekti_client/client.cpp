80144d69b8c7ee2b951ee7d253f33e797cdc0a92
ermallimaj <ermallimaj32@gmail.com>
2023-11-18 18:42:08 +0100

    File for the client

---
+++ Projekti_client/Projekti_client/client.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <winsock2.h>
+using namespace std;
+
+#pragma comment(lib,"ws2_32.lib") 
+#pragma warning(disable:4996) 
+
+#define SERVER "127.0.0.1"  // or "localhost" - ip address of UDP server
+#define BUFLEN 512  // max length of answer
+#define PORT 8888  // the port on which to listen for incoming data
+
+int main()
+{
+
+	return 0;
+}
