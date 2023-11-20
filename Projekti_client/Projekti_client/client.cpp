54ca33c0a9f17f9df254f802c0dce8f4a0d841f4
Erza Merovci <erzaamerovci@gmail.com>
2023-11-20 19:06:44 +0100

    User Role selection

--- Projekti_client/Projekti_client/client.cpp
+++ Projekti_client/Projekti_client/client.cpp
@@ -10,25 +10,41 @@ using namespace std;
 #define PORT 8888
 
 int main() {
-    system("title UDP C++ Client Gr16");
+    system("title UDP Client");
     WSADATA ws;
-    printf("Initialising Winsock...");
+    cout << "Initialising Winsock..." << endl;
     if (WSAStartup(MAKEWORD(2, 2), &ws) != 0) {
-        printf("Failed. Error Code: %d", WSAGetLastError());
+        cout << "Failed.Error Code:" << WSAGetLastError() << endl;
         return 1;
     }
-    printf("Initialised.\n");
+    cout<<"Initialised"<<endl;
 
     sockaddr_in server;
     int client_socket;
     if ((client_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == SOCKET_ERROR) {
-        printf("socket() failed with error code: %d", WSAGetLastError());
-        return 2;
+        cout << "socket() failed with error code:" << WSAFetLastError() << endl;
+           return 2;
     }
 
-    memset((char*)&server, 0, sizeof(server));
+    memset((char)&server, 0, sizeof(server));
     server.sin_family = AF_INET;
     server.sin_port = htons(PORT);
     server.sin_addr.S_un.S_addr = inet_addr(SERVER);
+
+    while (true)
+    {
+        char message[BUFLEN];
+        cout << "Select how do you want to continue \n As guest enter guest \n As admin write code \n ->";
+        cin.getline(message, BUFLEN);
+
+        if (sendto(client_socket, message, strlen(message), 0, (sockaddr)&server, sizeof(sockaddr_in)) == SOCKET_ERROR)
+        {
+            cout << "sendto() failed with error code:" << WSAGetLastError() << endl;
+            return 3;
+        }
+    }
+
+    closesocket(client_socket);
+    WSACleanup();
 
 }
