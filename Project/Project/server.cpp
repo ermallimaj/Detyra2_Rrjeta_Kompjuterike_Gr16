2ea19be034cdab1b1aa06619051f53185315e326
ermallimaj <ermallimaj32@gmail.com>
2023-11-20 18:50:18 +0100

    Basic Client Interaction

--- Project/Project/server.cpp
+++ Project/Project/server.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 #pragma comment(lib,"ws2_32.lib")
 #pragma warning(disable:4996)
-#define IP "127.0.0.1"
+#define SERVER "127.0.0.1"
 #define BUFLEN 512
 #define PORT 8888
 
 int main()
 {
-    system("title UDP C++ Server Gr16");
+    system("title UDP Server");
 
     sockaddr_in server, client;
 
@@ -28,14 +28,13 @@ int main()
     if ((server_socket = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
     {
         printf("Could not create socket: %d", WSAGetLastError());
+        exit(EXIT_FAILURE);
     }
     printf("Socket created.\n");
-
 
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(PORT);
-
 
     if (bind(server_socket, (sockaddr*)&server, sizeof(server)) == SOCKET_ERROR)
     {
@@ -44,4 +43,25 @@ int main()
     }
     puts("Bind done.");
 
+    while (true)
+    {
+        printf("Waiting for data... \n");
+        fflush(stdout);
+        char message[BUFLEN] = {};
+        int message_len;
+        int slen = sizeof(sockaddr_in);
+        if ((message_len = recvfrom(server_socket, message, BUFLEN, 0, (sockaddr*)&client, &slen)) == SOCKET_ERROR)
+        {
+            printf("recvfrom() failed with error code: %d", WSAGetLastError());
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Received packet from %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+        printf("Data: %s\n", message);
+
+        cin.getline(message, BUFLEN);
+    }
+
+    closesocket(server_socket);
+    WSACleanup();
 }
